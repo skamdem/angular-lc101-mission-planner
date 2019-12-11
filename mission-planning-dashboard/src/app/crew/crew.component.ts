00f4cf7d73450097bb7f6c12836d3937f16da131
sylvain kamdem <sylvain.kamdem@gmail.com>
2019-12-11 13:20:58 -0500

    end of angular part 1 after bonus mission

--- mission-planning-dashboard/src/app/crew/crew.component.ts
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
-
+  crew: string[] = ["Sylvain K", "Raja Chari", "Jasmin Moghbeli"];
   constructor() { }
 
   ngOnInit() {
