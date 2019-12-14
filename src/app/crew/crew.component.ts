209f189299f256cec27e4a62e7463af69905dcb3
sylvain kamdem <sylvain.kamdem@gmail.com>
2019-12-14 16:11:47 -0500

    completed studio before bonus missions

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -6,16 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
-
+  editingCrewMember: boolean[]= [];
+  
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
     {name: "Ellen Ochoa", firstMission: true}
   ];
-
-  constructor() { }
-
-  ngOnInit() {
+  
+  constructor() {
+    //this.initEditingCrewMember();
+    for (let i = 0; i< this.crew.length; i++){
+      this.editingCrewMember.push(false);
+    }
   }
 
+  ngOnInit() {
+    
+  }
+
+  /*initEditingCrewMember(){
+    for (let i = 0; i< this.crew.length; i++){
+      this.editingCrewMember.push(false);
+    }
+  }*/
+
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+    this.editingCrewMember.push(false);
+  }
+
+  remove(member: object) {
+    let index: number = this.crew.indexOf(member);
+    if (index > -1) {
+      this.crew.splice(index, 1);
+      this.editingCrewMember.splice(index, 1);
+    }
+  }
+
+  updateCrewMember(member : object) {
+    let index: number = this.crew.indexOf(member);
+    this.crew[index] = member;
+    this.editingCrewMember[index] = false;
+  }
 }
