30424a9679cd61eca157b74caa91cc080177681f
sylvain kamdem <sylvain.kamdem@gmail.com>
2019-12-17 16:48:38 -0500

    completed studio before Bonus mission

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class CrewComponent implements OnInit {
 
   inCrew: boolean = false;
+  crewIsFull: boolean = false;
   crew: object[] = [];
+  activateMember: boolean[] = [];
+  hovered : string ='';
 
   candidates: object[] = [
     {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
@@ -20,10 +23,42 @@ export class CrewComponent implements OnInit {
     {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
   ];
 
-  constructor() { }
+  constructor() {
+    for(let i = 0; i < this.candidates.length; i++) {
+      this.activateMember.push(false);
+    }
+  }
 
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
+  addCrewMember(candidate:object){
+    let index:number = -1;
+    let indexInCandidates:number = 0;
+    for(let i = 0; i < this.crew.length; i++) {
+      if (this.crew[i]['name'] === candidate['name']) {
+        index = i;
+        break;
+      }
+    }
+
+    for(let i = 0; i < this.candidates.length; i++) {
+      if (this.candidates[i]['name'] === candidate['name']) {
+        indexInCandidates = i;
+        break;
+      }
+    }
+
+    if (index !== -1) {//found
+      //If the candidate is already part of the crew, then their data should be removed from the crew array
+      this.crew.splice(index,1);
+      this.activateMember[indexInCandidates] = false;
+    } else if (this.crew.length <= 2){
+      //If the crew size is less than 3 AND the candidate is not part of the crew, then their data should be added to the crew array.
+      this.crew.push(candidate);
+      this.activateMember[indexInCandidates] = true;
+    }
+    this.crewIsFull = (this.crew.length == 3);
+  }
 
 }
